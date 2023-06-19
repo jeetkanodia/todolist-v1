6f28b2825653c1ddc208556413a656833097fda9
jeetkanodia <jeetkanodia@gmail.com>
2023-06-20 00:06:41 +0530

    views added

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 
-const app = express();
+const app = express(); 
+
+var items = ["Buy Food", "Cook Food" , "Eat Food"]; 
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine' , 'ejs');
 
 app.get('/' , (req,res)=>{
     var today = new Date();
-    var day = "";
-    var currentDay = today.getDay();
-    switch(currentDay)
-    {
-        case 0: day = "Sunday";break;
-        case 1: day = "Monday";break;
-        case 2: day = "Tuesday";break;
-        case 3: day = "Wednesday";break;
-        case 4: day = "Thrusday";break;
-        case 5: day = "Friday";break;
-        case 6: day = "Saturday";break;
-        default: day = "Error Occured"; 
-    }
-    res.render('list' , {kindOfDay : day})
+    var options={
+        weekday: 'long',
+        day: "numeric",
+        month: 'long'
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+    
+    res.render('list' , {kindOfDay : day , newListItems: items})
 });
+
+app.post('/' , (req,res)=>{
+    var item = req.body.newItem;
+    if(item!="")
+    items.push(item);
+    res.redirect('/');
+})
+
 
 app.listen(3000 , ()=>{
     console.log("Server up on port 3000");
