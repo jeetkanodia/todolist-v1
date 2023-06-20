95ab680c6d8c1a68fc231169fd6c149098abebda
jeetkanodia <jeetkanodia@gmail.com>
2023-06-20 16:38:30 +0530

    work route added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express(); 
 
+var workItems = [];
 var items = ["Buy Food", "Cook Food" , "Eat Food"]; 
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,16 +20,36 @@ app.get('/' , (req,res)=>{
 
     var day = today.toLocaleDateString("en-US", options);
     
-    res.render('list' , {kindOfDay : day , newListItems: items})
+    res.render('list' , {listTitle : day , newListItems: items})
 });
 
 app.post('/' , (req,res)=>{
+    
     var item = req.body.newItem;
-    if(item!="")
-    items.push(item);
-    res.redirect('/');
+    if(req.body.list === "Work")
+    {
+        workItems.push(item);
+        res.redirect('/work')
+    }
+    else{
+        if(item!="")
+        items.push(item);
+        res.redirect('/');
+    }
+    
 })
 
+
+app.get('/work' , (req,res)=>{
+    res.render('list' , {listTitle : "Work List" , newListItems: workItems})
+})
+
+app.post('/work' , (req,res)=>{
+    var item = req.body.newItem;
+    if(item!="")
+    workItems.push(item);
+    res.redirect('/work');
+})
 
 app.listen(3000 , ()=>{
     console.log("Server up on port 3000");
